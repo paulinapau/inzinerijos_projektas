59438a8fba8591e18fe4fdee5efe1fdd86137c5f
Paulius Skliaudys <paulius.skliaudys@ktu.edu>
2022-02-18 13:57:59 +0200

    fixed bug

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -109,7 +109,7 @@ function ChatMessage(props){
   return (
     <div className={'message ${messageClass}'}>
       <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
-      <p>t{text}</p>
+      <p>{text}</p>
       </div>
   )
 }
