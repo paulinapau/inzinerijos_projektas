ee9f9c96f30f8ec00e1eb0eb70e64526809c559a
zu-do <61902997+zu-do@users.noreply.github.com>
2022-02-18 12:21:57 +0200

    Update App.tsx

--- chat-web/src/App.tsx
+++ chat-web/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>EIKITE NAXUJ</p>
+        <p>laba diena</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
