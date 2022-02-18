7c380ac3d0922558d335bfdf2b1fa9c5361e3d6c
paulinapau <paulinapozeraite@gmail.com>
2022-02-18 11:54:28 +0200

    Update App.tsx

--- chat-web/src/App.tsx
+++ chat-web/src/App.tsx
@@ -4,9 +4,12 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Sveiki, visi
+        </h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
